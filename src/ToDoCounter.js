82defe1d914e92b9c1313eafbf628cafce7f0359
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-05-31 22:42:36 -0300

    first day learning react on platzi

---
+++ src/ToDoCounter.js
@@ -0,0 +1,9 @@
+function ToDoCounter (){
+    return(
+      <h1>
+        Has completado x de x tareas.
+      </h1>
+    );
+}
+
+export {ToDoCounter};
---
+++ src/ToDoItem.js
@@ -0,0 +1,11 @@
+function ToDoItem (){
+    return(
+      <li>
+        <span>V</span>
+        <p>Tarea bla bla bla</p>
+        <span>X</span>
+      </li>
+    );
+}
+
+export {ToDoItem};
