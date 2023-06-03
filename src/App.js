63be2937aa678f48a05a9b5676842cf6b6f6852c
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-06-03 07:30:05 -0300

    style config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
 import { ToDoCounter } from './ToDoCounter';
 import { ToDoSearch } from './ToDoSearch';
 import { ToDoList } from './ToDoList';
 import { ToDoItem } from './ToDoItem';
 import { CreateToDoButton } from './CreateToDoButton';
+import React from 'react';
 
+const defaultToDos = [
+  { text : 'Cortar cebolla' , completed : true},
+  { text : 'Cantar bad bunny' , completed : true}
+]
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       
       <ToDoCounter completed="2" total="5" />
       <ToDoSearch />
       <ToDoList>
 
-        <ToDoItem/>
-        <ToDoItem/>
-        <ToDoItem/>
+        {defaultToDos.map(todo =>(
+          <ToDoItem key={todo.text} text={todo.text} />
+        ))}
+
       </ToDoList>
 
       <CreateToDoButton />
-    </div>
+    </React.Fragment>
   );
 }
 
