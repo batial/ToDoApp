608ce663789beec03992079373993bc7b559b104
Sebastián Debesa <sebastian.debesa@hotmail.com>
2023-06-07 15:04:21 -0300

    adding some styles when the to-dos are completed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { ToDoCounter } from './ToDoCounter';
-import { ToDoSearch } from './ToDoSearch';
-import { ToDoList } from './ToDoList';
-import { ToDoItem } from './ToDoItem';
-import { CreateToDoButton } from './CreateToDoButton';
-import React from 'react';
-import './index.css';
+import { ToDoCounter } from "./ToDoCounter";
+import { ToDoSearch } from "./ToDoSearch";
+import { ToDoList } from "./ToDoList";
+import { ToDoItem } from "./ToDoItem";
+import { CreateToDoButton } from "./CreateToDoButton";
+import React from "react";
+import "./index.css";
 
 const defaultToDos = [
-  { text : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' , completed : true},
-  { text : 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' , completed : true},
-  { text : 'Cortar cebolla' , completed : true},
-]
+  {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    completed: true,
+  },
+  {
+    text: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    completed: true,
+  },
+  { text: "Cortar cebolla", completed: true },
+];
 
 function App() {
   return (
     <>
-      
       <ToDoCounter completed="2" total="5" />
       <ToDoSearch />
       <ToDoList>
-
-        {defaultToDos.map(todo =>(
-          <ToDoItem key={todo.text} text={todo.text} />
+        {defaultToDos.map((todo) => (
+          <ToDoItem key={todo.text} text={todo.text} completed={todo.completed} />
         ))}
-
       </ToDoList>
 
       <CreateToDoButton />
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,9 +1,9 @@
 function ToDoItem(props) {
   return (
     <li className="toDoItem" >
+      <span className="completedItem">V</span>
+      <p className={`toDoP ${props.completed && 'toDoItemComplete'}`}>{props.text}</p>
       <span className="eraseItem">X</span>
-      <p>{props.text}</p>
-      {/* <span>V</span> */}
     </li>
   );
 }
